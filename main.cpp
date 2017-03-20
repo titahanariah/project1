11cc49c6f8f38ade48b40cd4218c9e733b8cef5d
titahanariah <titahanariah@users.noreply.github.com>
2017-03-20 12:27:17 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+   cout << "tita hanariah";
+   cout << "belajaraja";
+   return 0;
+   }
---
+++ main2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main () {
+	cout << "hello word";
+	cout << "hello word plus enter" << endl;
+	
+	
+	return 0;
+}
